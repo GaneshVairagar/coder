b6ec72eb2e1c85862364194d98a491f26deed5e6
GaneshVairagar <vairagarganesh7498@gmail.com>
2024-04-05 12:58:25 +0530

    4 time

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  
+};
+
+module.exports = {
+  // Your Next.js configuration
+  distDir: '.next', // This is the default, but make sure it's not changed
+  // Specify the export path
+  outDir: 'out',
 };
 
 export default nextConfig;
